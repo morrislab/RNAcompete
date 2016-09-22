da6d4415a8f38cb0a851f831bf7e2b8f4788a1f2
Kate Nie <kunnie525@gmail.com>
2016-09-22 03:01:20 -0400

    update scripts.sh

--- Normalization/normalization_scripts/scripts.sh
+++ Normalization/normalization_scripts/scripts.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-   /opt/sw/matlab/bin/matlab -nodesktop -nojvm -nodisplay -r " load_data_from_single_file('raw_data.txt'); exit;"
-   /opt/sw/matlab/bin/matlab -nodesktop -nojvm -nodisplay -r " script_final('col', 'quant', 'trim', 5); exit;"
-# submitjob /opt/sw/matlab2010a/bin/matlab -nodesktop -nojvm -nodisplay -r " script_human_final('col', 'quant', 'trim', 5); exit;"
-#submitjob   /opt/sw/matlab2010a/bin/matlab -nodesktop -nojvm -nodisplay -r " script_human_final('col', 'quant', 'median', 0); exit;"
-
-#  /opt/sw/matlab2010a/bin/matlab -nodesktop -nojvm -nodisplay -r " script_human_final('col', 'matlab_median', 'trim', 0);exit;"
-# submitjob   /opt/sw/matlab2010a/bin/matlab -nodesktop -nojvm -nodisplay -r " script_human_final('col', 'matlab_median', 'trim', 5);exit;"
- # /opt/sw/matlab2010a/bin/matlab -nodesktop -nojvm -nodisplay -r " script_human_final('col', 'matlab_median', 'median', 0);exit;"
+matlab -nodesktop -nojvm -nodisplay -r " load_data_from_single_file('raw_data.txt'); exit;"
+matlab -nodesktop -nojvm -nodisplay -r " script_final('col', 'quant', 'trim', 5); exit;"
